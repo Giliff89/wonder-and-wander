cdf40f2f1743a32b74667ab592e5885acea1af25
Gina Iliff <giliff@me.com>
2017-09-18 12:22:36 -0700

    Initial Commit

---
+++ Wonder and Wander/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wonder and Wander
+//
+//  Created by gina iliff on 9/18/17.
+//  Copyright © 2017 gina iliff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
