8f9aeae931e645673dbd66916ccee4a11dd3a5dc
Gina Iliff <giliff@me.com>
2017-09-18 13:56:48 -0700

    Set up Firebase, initial (Auth) View Controller

---
+++ Pods/Target Support Files/Pods-Wonder and Wander/Pods-Wonder and Wander-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wonder_and_Wander : NSObject
+@end
+@implementation PodsDummy_Pods_Wonder_and_Wander
+@end
---
+++ Wonder and Wander/HomeViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Wonder and Wander
+//
+//  Created by gina iliff on 9/18/17.
+//  Copyright © 2017 gina iliff. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var loginMode = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func topTapped(_ sender: Any) {
+        if let email = emailTextField.text {
+            if let password = passwordTextField.text {
+                if loginMode {
+                    Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                        if let error = error {
+                            print(error)
+                        } else {
+                            self.performSegue(withIdentifier: "authToProfile", sender: nil)
+                        }
+                    })
+                    
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if let error = error {
+                            print(error)
+                        } else {
+                            if let user = user {
+                            Database.database().reference().child("users").child(user.uid).child("email").setValue(email)
+                                self.performSegue(withIdentifier: "authToProfile", sender: nil)
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if loginMode {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Login", for: .normal)
+            loginMode = false
+            
+        } else {
+            topButton.setTitle("Login", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            loginMode = true
+        }
+    }
+    
+    // Hide keyboard when user touches outside text box
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
